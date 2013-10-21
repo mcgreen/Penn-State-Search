18597b69ac7ac523d2362293959ff0771d5785ce
mcgreen <mcg5132@pollock201-lab-mac227.clc-labs.its.psu.edu>
2013-10-21 17:19:05 -0400

    coredatamodel
    adding core data model

---
+++ Penn State Search/MyDataManager.h
@@ -0,0 +1,13 @@
+//
+//  MyDataManager.h
+//  Penn State Search
+//
+//  Created by MICHAEL COLE GREEN on 10/21/13.
+//  Copyright (c) 2013 MICHAEL COLE GREEN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyDataManager : NSObject
+
+@end
---
+++ Penn State Search/MyDataManager.m
@@ -0,0 +1,14 @@
+//
+//  MyDataManager.m
+//  Penn State Search
+//
+//  Created by MICHAEL COLE GREEN on 10/21/13.
+//  Copyright (c) 2013 MICHAEL COLE GREEN. All rights reserved.
+//
+
+#import "MyDataManager.h"
+#import "DataManager.h"
+
+@implementation MyDataManager
+
+@end
